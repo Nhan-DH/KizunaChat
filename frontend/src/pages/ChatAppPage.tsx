b29a04cd83e22bf90c3566b02bf6c7557af23fb6
Nhan-DH <hongnhan474747@gmail.com>
2026-05-27 13:35:04 +0700

    test refresh token

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,13 +1,24 @@
 import Logout from "@/components/auth/Logout";
+import { Button } from "@/components/ui/button";
+import api from "@/lib/axios";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { toast } from "sonner";
 
 const ChatAppPage = () => {
     const user = useAuthStore((s) => s.user);
-
+    const handleOnClick = async () => {
+        try {
+            await api.get("/users/test", { withCredentials: true });
+            toast.success("Test thành công");
+        } catch (error) {
+            toast.error("Test thất bại");
+        }
+    };
     return (
         <div>
             {user?.username}
             <Logout />
+            <Button onClick={handleOnClick}>Test</Button>
         </div>
     );
 };
