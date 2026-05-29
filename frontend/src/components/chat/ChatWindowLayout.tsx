42d4827f0676a96b185e327d9c9a55820fdea16f
Nhan-DH <hongnhan474747@gmail.com>
2026-05-30 00:04:36 +0700

    feat : layout message

--- frontend/src/components/chat/ChatWindowLayout.tsx
+++ frontend/src/components/chat/ChatWindowLayout.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useChatStore } from "@/stores/useChatStore";
+import { SidebarInset } from "../ui/sidebar";
+import ChatWelcomeScreen from "./ChatWelcomScreen";
+import ChatWindowSkeleton from "./ChatWindowSkeleton";
+import ChatWindowHeader from "./ChatWindowHeader";
+import ChatWindowBody from "./ChatWindowBody";
+import MessageInput from "./MessageInput";
+
+
 
 const ChatWindowLayout = () => {
-  return <div>ChatWindowLayout</div>;
+
+  const activeConversationId = useChatStore((s) => s.activeConversationId);
+  const conversations = useChatStore((s) => s.conversations);
+  const loading = useChatStore((s) => s.messageLoading);
+
+  const selectedConvo =
+    conversations.find((c) => c._id === activeConversationId) ?? null;
+
+  if (!selectedConvo) {
+    return <ChatWelcomeScreen />;
+  }
+
+  if (loading) {
+    return <ChatWindowSkeleton />;
+  }
+
+  return (
+    <SidebarInset className="flex flex-col h-full flex-1 overflow-hidden rounded-sm shadow-md">
+      {/* Header */}
+      <ChatWindowHeader />
+
+      {/* Body */}
+      <div className="flex-1 overflow-y-auto bg-primary-foreground">
+        <ChatWindowBody />
+      </div>
+
+      {/* Footer */}
+      <MessageInput />
+    </SidebarInset>
+  );
 };
 
 export default ChatWindowLayout;
