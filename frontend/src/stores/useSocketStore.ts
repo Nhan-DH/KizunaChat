527716a78a66496b6c24b2dfbf257b3cc5cf750f
Nhan-DH <hongnhan474747@gmail.com>
2026-05-31 16:57:09 +0700

    feat : create new group chat

--- frontend/src/stores/useSocketStore.ts
+++ frontend/src/stores/useSocketStore.ts
@@ -72,6 +72,11 @@ export const useSocketStore = create<SocketState>((set, get) => ({
 
             useChatStore.getState().updateConversation(updated);
         });
+        // new group chat
+        socket.on("new-group", (conversation) => {
+            useChatStore.getState().addConvo(conversation);
+            socket.emit("join-conversation", conversation._id);
+        });
     },
     disconnectSocket: () => {
         const socket = get().socket;
@@ -80,4 +85,5 @@ export const useSocketStore = create<SocketState>((set, get) => ({
             set({ socket: null });
         }
     },
+
 }));
